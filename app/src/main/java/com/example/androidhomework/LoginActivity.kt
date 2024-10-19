c12ed1d1cdc0b70c1b7564c2ed850e625dd83f2c
VALDI-N1 <vladbovtruchenok@gmail.com>
2024-10-19 03:39:12 +0300

    feat: added homework16

--- app/src/main/java/com/example/androidhomework/LoginActivity.kt
+++ app/src/main/java/com/example/androidhomework/LoginActivity.kt
@@ -2,21 +2,25 @@ package com.example.androidhomework
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        startActivity(Intent(this, SplashScreen::class.java))
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            startActivity(Intent(this, NotesActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/androidhomework/OnboardingScreen3.kt
+++ app/src/main/java/com/example/androidhomework/OnboardingScreen3.kt
@@ -1,22 +1,29 @@
 package com.example.androidhomework
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class OnboardingScreen3 : AppCompatActivity() {
+    private lateinit var startedButton: Button
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding_screen3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        startActivity(Intent(this, SplashScreen::class.java))
+        startedButton = findViewById(R.id.Button3)
+            startedButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
